bea7530e6ccf4ec9f588ac079a0318218be869b9
Christophe Jolif <christophe.jolif@fr.ibm.com>
2015-04-27 11:41:45 +0200

    add unit test

--- tests/unit/Bar.js
+++ tests/unit/Bar.js
@@ -3,10 +3,10 @@ define([
 	"intern/chai!assert",
 	"delite/register",
 	"deliteful/Bar"
-], function (registerSuite, assert, register) {
-	var container, node;
+], function (registerSuite, assert, register, Bar) {
+	var container, mknode, pnode;
 	var htmlContent =
-		"<d-bar id='dbar' vertical='false' style='width:999px; height:999px'>" +
+		"<d-bar id='dbar'>" +
 			"<div id='divA' class='fill'>A</div><div id='divB' class='fill'>B</div>" +
 			"<div id='divC' class='fill'>C</div></d-bar>";
 	registerSuite({
@@ -15,11 +15,22 @@ define([
 			container = document.createElement("div");
 			document.body.appendChild(container);
 			container.innerHTML = htmlContent;
-			register.parse(container);
-			node = document.getElementById("dbar");
-			node.deliver();
+			pnode = new Bar();
+			container.appendChild(pnode);
+			register.deliver();
+			mknode = document.getElementById("dbar");
 		},
-		"test" : function () {
+		"afterinit" : function () {
+			assert.strictEqual(mknode.vertical, false);
+			assert.strictEqual(pnode.vertical, false);
+		},
+		"switchtovertical" : function () {
+			mknode.vertical = true;
+			pnode.vertical = true;
+			mknode.deliver();
+			pnode.deliver();
+			assert.strictEqual(mknode.vertical, true);
+			assert.strictEqual(pnode.vertical, true);
 		},
 		teardown : function () {
 			container.parentNode.removeChild(container);
